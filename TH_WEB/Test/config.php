d7ed89372c463b325c6948dbd10831292188e77f
Lê Minh Tú <tule.31191024175@st.ueh.edu.vn>
2022-05-26 00:55:00 +0700

    Delete

--- TH_WEB/Test/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	define("HOST", "localhost");
-	define("DB", "db_hanghoa");
-	define("USER", "root");
-	define("PASSWORD", "");
-    define("NUM_PROD_PER_PAGE", 5);
-
-?>
--- TH_WEB/Test/script.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector('#menu-bar');
-let nav = document.querySelector('.nav');
-
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    nav.classList.toggle('active');
-}
